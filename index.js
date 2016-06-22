45ed3b9ab69231682a9d22434d972b425fa25f0a
Jordan Sexton <jordan@jordansexton.com>
2016-06-22 15:16:00 -0500

    .dockerignore doesn't work

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 'use strict';
 
-describe('convox bugs', function () {
-    it("has bug reproductions in separate branches");
+var fs     = require('fs');
+var path   = require('path');
+var expect = require('chai').expect;
+
+describe('convox start', function () {
+    it(".dockerignore doesn't work", function () {
+        var readFile = function () {
+            var file   = path.join(__dirname, 'secrets', 'secret.txt');
+            return fs.readFileSync(file, 'utf8');
+        };
+        expect(readFile).to.throw(/ENOENT/);
+    });
 });
